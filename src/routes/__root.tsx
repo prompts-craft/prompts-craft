47170f2e84cfe6c07bc1020726589e7b6aa83fa5
prompts-craft <aliahmadg8575@gmail.com>
2026-08-14 08:03:40 +0500

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -89,6 +89,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+      { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Audiowide&display=swap" },
